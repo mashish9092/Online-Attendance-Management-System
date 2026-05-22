8d78351ff0f36466427e70042859e0870ad240e4
Ashish Mehra <mashish9092@gmail.com>
2026-05-22 11:56:53 +0530

    Fixed repository structure

--- obj/Debug/net8.0/AttendanceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AttendanceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a53d4517f3dbc7b77a3cf1e31d17dbece87be8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b90e8abb5fcd6ea95427cd7a927d5f33cf75134")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
